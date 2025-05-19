8bfaab46d7803a18b8b1101304a861d787a3523b
Matheus <1410mlg@gmail.com>
2025-05-19 14:04:06 -0300

    Projeto finalizado

--- movies-lib/src/main.jsx
+++ movies-lib/src/main.jsx
@@ -1,24 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import App from './App.jsx'
-import Home from './pages/Home.jsx';
-import Movie from './pages/Movie.jsx';
-import Search from './pages/Search.jsx';
+import App from "./App";
+import Home from "./pages/Home";
+import Movie from "./pages/Movie";
+import Search from "./pages/Search";
 
-import './index.css'
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route element={<App />}>
-          <Route path='/' element={<Home />} />
-          <Route path='movie/:id' element={<Home />} />
-          <Route path='search' element={<Home />} />
+          <Route path="/" element={<Home />} />
+          <Route path="movie/:id" element={<Movie />} />
+          <Route path="search" element={<Search />} />
         </Route>
       </Routes>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
